01f158e79532bf3e4887d5d014f8340835a1b627
Jacob Hood <jakelhood@gmail.com>
2021-07-07 20:23:19 -0700

    minor fixes

---
+++ Objects/ChallongeMessage.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Challonge.Objects
+{
+    internal class ChallongeMessage : ChallongeObject
+    {
+        [JsonProperty("message")]
+        internal string Message { get; set; }
+    }
+}
